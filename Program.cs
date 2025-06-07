170b0baaf18b0f3aad7d45f70db9d7f7d501711d
Abrar Rakin <rakinabrar151@gmail.com>
2025-06-07 23:59:15 +0600

    Build Service for API Configuration

--- Program.cs
+++ Program.cs
@@ -1,9 +1,25 @@
+
 using Microsoft.AspNetCore.Mvc;
+
+using asp_net_ecommerce_web_api.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Add Services to the Controller
+
+// Add services to the controller 
 builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+  options.InvalidModelStateResponseFactory = context =>
+  {
+    var errors = context.ModelState
+                    .Where(e => e.Value != null && e.Value.Errors.Count > 0)
+                    .SelectMany(e => e.Value?.Errors != null ? e.Value.Errors.Select(x => x.ErrorMessage) : new List<string>()).ToList();
+    return new BadRequestObjectResult(ApiResponse<object>.ErrorResponse(errors, 400, "Validation failed"));
+  };
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -11,19 +27,24 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
+
 app.UseHttpsRedirection();
 
 
-app.MapGet("/", () => "Yes, API is Perfectly Calling");
-
+app.MapGet("/", () => "Api is working fine");
 
 app.MapControllers();
 app.Run();
-//CRUD 
-//Create => Create a Category -->POST: /api/categories
-//Read => Read a Category   --> GET: /api/categories
-//Update => Update a Category  -->PUT: /api/categories
-//Delete => Delete a Category  --> DELETE: /api/categories
+
+
+
+// CRUD 
+// Create => Create a Category => POST : /api/categories 
+// Read => Read a Category => GET : /api/categories 
+// Update => Update a Category => PUT : /api/categories 
+// Delete => Deelete a Category => Delete : /api/categories 
+
+// MVC = Models, Views, Controllers
