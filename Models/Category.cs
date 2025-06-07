d176d770cb93a23d355d3f070e2a4954fac51b2c
Abrar Rakin <rakinabrar151@gmail.com>
2025-06-07 23:47:02 +0600

    Some Error Fix

--- Models/Category.cs
+++ Models/Category.cs
@@ -8,7 +8,7 @@ namespace asp_net_ecommerce_web_api.Models
     public class Category
     {
         public Guid CategoryId { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Description { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
     };
